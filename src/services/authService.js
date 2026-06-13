82eb58662267a1d2a919e2fbb12fe4fb2f5d5cc9
Aman Yadav <yadavamanindra123@gmail.com>
2026-06-13 22:47:30 +0530

    fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://fooddeliveryapp-backend-production-33e8.up.railway.app/api";
 
 export const loginUser = async (data) => {
   const response = await axios.post(`${BASE_URL}/auth/login`, data);
