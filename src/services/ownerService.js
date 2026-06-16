e37f73da56c092e6bf253c42d70047739e45582e
Amanyadav9876 <yadavamanindra123@gmail.com>
2026-06-16 13:28:37 +0530

    Update ownerService.js

--- src/services/ownerService.js
+++ src/services/ownerService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://fooddeliveryapp-backend-production-33e8.up.railway.app/api";
 
 const getToken = () => localStorage.getItem("token");
 
